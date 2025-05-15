500c58776f8b8b06694d59fc57c9f7e1683e7685
Didikkurniawan3 <didik2584@gmail.com>
2025-05-15 16:07:58 +0700

    test pass

--- src/components/comment/create-comment.jsx
+++ src/components/comment/create-comment.jsx
@@ -29,7 +29,7 @@ function CreateComment({ createComment }) {
           className="btn btn-success float-end mt-2"
           onClick={onCommentSubmit}
         >
-          Pol
+          Post
         </button>
       </div>
     </div>
